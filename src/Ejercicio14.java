27b6b04e38be179329b6567aa91f5fcbe8851650
Prixen <prixenlol@gmail.com>
2022-11-06 16:00:47 +0100

    aa

--- src/Ejercicio14.java
+++ src/Ejercicio14.java
@@ -1,2 +1,30 @@
+/*Realiza un método que devuelva cierto si la matriz de dimensiones NxN que se le pasa como argumento es simétrica.
+ Una matriz A es simétrica si a(i,j) = a(j,i). Ejemplos de matrices simétricas en las imágenes*/
+import java.util.Scanner;
 public class Ejercicio14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca la cantidad de filas y columnas en la matriz: ");
+        int fc = sc.nextInt();
+        int [][] matriz = new int[fc][fc];
+        LLenaMatriz(matriz);
+        System.out.println(CompruebaSimetrica(matriz));
+    }
+    public static void LLenaMatriz(int[][] matriz){
+        Scanner sc = new Scanner(System.in);
+        for(int f=0 ; f< (matriz.length  ) ; f++){
+            for(int c= 0 ; c< (matriz.length ) ; c++){
+                System.out.println("Introduzca el valor de la fila nº" + (f+1) +" en la columna nº" + (c+1));
+                matriz[f][c] = sc.nextInt();
+            }
+        }
+    }
+    public static boolean CompruebaSimetrica(int[][] matriz){
+        for (int f = 0; f < (matriz.length  ) ; f++){
+            for(int c = 0; c < (matriz.length  ); c++){
+                if ( matriz[f][c] != matriz[c][f] ) {  return false;}
+            }
+        }
+        return true;
+    }
 }
---
+++ src/Ejercicio15.java
@@ -0,0 +1,14 @@
+/*Realiza un método que reciba una matriz de dimensiones NxN y almacene sobre ella su traspuesta.
+ La matriz traspuesta es aquella que se obtiene cambiando ordenadamente las filas por las columnas.
+ En la imagen aparece como ejemplo una matriz y su traspuesta.
+*   2   3   0
+*   1   2   0
+*   3   5   6
+*
+*   2   1   3
+*   3   2   5
+*   0   0   6
+*
+* */
+public class Ejercicio15 {
+}
